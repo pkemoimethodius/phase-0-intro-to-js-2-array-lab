7622d6b5e89c0f54070d93da877ad73130ea87ce
pkemoi methodius <pkemoimethodius@gmail.com>
2024-07-01 23:48:35 +0300

    Adds the array functions

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    const catsDestroyed =cats.push("Ralph");
+      return catsDestroyed;
+  }
+
+  function destructivelyPrependCat(){
+    const catsDestroyed2 =cats.unshift("Bob");
+    return catsDestroyed2;
+  }
+
+  function destructivelyRemoveLastCat(){
+    const catsDestroyed3 =cats.pop();
+    return catsDestroyed3;
+  }
+
+
+  function destructivelyRemoveFirstCat(){
+    const catsDestroyed4 =cats.shift();
+    return catsDestroyed4;
+  }
+
+
+  function appendCat(){
+    const catsShallow = [...cats,"Broom"];
+    return catsShallow;
+  }
+
+  function  prependCat(){
+    const catsShallow2 = ["Arnold",...cats];
+    return catsShallow2;
+  }
+
+  function removeLastCat(){
+    const catsShallow3 =cats.slice(0,-1);
+    return catsShallow3;
+  }
+
+  function removeFirstCat(){
+    const catsShallow4 = cats.slice(1);
+    return catsShallow4;
+  }
